a3b0a55f7bc4dffc33c60044dcbe7a8953f1b083
S48AHan <saberahmedn2@gmail.com>
2026-07-17 19:51:05 +0600

    day 4

---
+++ day4/server.js
@@ -0,0 +1,73 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  console.log("URL:", req.url);
+  console.log("Method:", req.method);
+
+  //   res.setHeader("Content-Type", "text/html");
+
+  //   res.write("<html>");
+  //   res.write("<head><title>Saber er Server</title></head>");
+  //   res.write("<body><h1>Saber er Server response!</h1></body>");
+  //   res.write("</html>");
+
+  if (req.url === "/dashboard") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+
+    res.end(`
+    <html>
+      <head>
+        <title>Dashboard</title>
+      </head>
+      <body>
+        <h1>Dashboard! ${PORT}</h1>
+      </body>
+    </html>
+  `);
+  } else if (req.url === "/about") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+
+    res.end(`
+    <html>
+      <head>
+        <title>about page</title>
+      </head>
+      <body>
+        <h1>About!</h1>
+      </body>
+    </html>
+  `);
+  } else {
+    res.writeHead(300, {
+      "Content-Type": "text/html",
+    });
+
+    res.end(`
+    <html>
+      <head>
+        <title>Saber er Server</title>
+      </head>
+      <body>
+        <h1>Onno jaygayi ghuiro na!</h1>
+        <form action="/submit-details" method="POST">
+          <label for="userName">Name</label>
+          <input type="text" name= "userName" placeholder = "Enter your name"/><br>
+          <label for="male">male</label>
+          <input type="radio" name= "gender" value="male"><br>
+          <label for="female">female</label>
+          <input type="radio" name= "gender" value="female"><br>
+          <input type  = "submit" value= "Submit"/>
+        </form >
+      </body>
+    </html>
+  `);
+  }
+});
+
+const PORT = 3003;
+server.listen(PORT, () => {
+  console.log(`Server Is running`);
+});
